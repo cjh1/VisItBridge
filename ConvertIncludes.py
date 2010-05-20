7499910f512b2f9bb20e32d792d6a8f38078748d
Robert Maynard <robert.maynard@kitware.com>
2010-05-20 16:11:09 -0400

    Adding in script to convert include paths
    VisIt uses < and > incorrectly when including files

---
+++ ConvertIncludes.py
@@ -0,0 +1,39 @@
+import os
+import fileinput
+#convert includes in VisIt
+
+FileNamesToClean = []
+
+def cleanHeaderIncludes( filename, filepath ):
+  fpath = os.path.join(filepath,filename)
+  for line in fileinput.input(fpath,inplace=1):  
+    if line.find("#include <") >= 0:
+      start = line.find("<")
+      end = line.find(">")      
+      #check and see if the file name is valid
+      name = line[start+1:end]      
+      if name in FileNamesToClean:      
+        line = line.replace("<","\"",1)
+        line = result.replace(">","\"",1)      
+    print line
+
+if __name__ == "__main__":
+  hidden = "."+os.sep+"."  
+  
+  #parse the first time to build the set of file names we can parse
+  for dirname,dirnames,filenames in os.walk('.'):         
+    if (dirname[0:3] == hidden):
+      #do not traverse hidden directores
+      continue
+    for filename in filenames:
+      FileNamesToClean.append(filename)
+      
+  #second time walk the files to clean them
+  for dirname,dirnames,filenames in os.walk('.'):         
+    if (dirname[0:3] == hidden):
+      #do not traverse hidden directores
+      continue
+    for filename in filenames:
+      cleanHeaderIncludes(filename,dirname)
+      
+      
