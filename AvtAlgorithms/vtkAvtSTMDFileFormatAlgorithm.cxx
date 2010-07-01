a059ede5b4f5c5320ce85334afbd6b98d21d4037
Robert Maynard <robert.maynard@kitware.com>
2010-07-01 11:58:01 -0400

    ENH: We now support CSGMesh input types.
    The CSGMesh is just an unstructured grid. Now that boost is properly working and the CSGGrid class is now compiling we should have no problem supporting these meshes.

--- AvtAlgorithms/vtkAvtSTMDFileFormatAlgorithm.cxx
+++ AvtAlgorithms/vtkAvtSTMDFileFormatAlgorithm.cxx
@@ -183,9 +183,7 @@ int vtkAvtSTMDFileFormatAlgorithm::RequestData(vtkInformation *request,
 
       switch(meshMetaData.meshType)
         {
-        case AVT_CSG_MESH:
-          vtkErrorMacro("Currently we do not support AVT_CSG_MESH.");
-          break;
+        case AVT_CSG_MESH:          
         case AVT_AMR_MESH:
         case AVT_RECTILINEAR_MESH:
         case AVT_CURVILINEAR_MESH:
